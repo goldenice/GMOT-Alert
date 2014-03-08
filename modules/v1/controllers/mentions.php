9725f77f1b94e93a72b8f6b604678b98b53d34b6
Rick Lubbers <ik@ricklubbers.nl>
2014-03-08 14:09:17 +0100

    Redefined interface

--- modules/v1/controllers/mentions.php
+++ modules/v1/controllers/mentions.php
@@ -2,11 +2,7 @@
 namespace Modules\V1\Controllers;
 
 class Mentions extends \System\Basecontroller {
-    function index($arg = null) {
-        // TODO: write
-    }
-    
-    function since($arg = null) {
-        // TODO: write
+    function get($arg = null) {
+        var_dump($arg);
     }
 }
